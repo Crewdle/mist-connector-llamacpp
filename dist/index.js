a37e88ed39dd2358c715b3d7fed66940c655d3cb
Vincent Lamanna <vincent@crewdle.com>
2024-05-25 16:20:03 -0400

    CA-2232 Export connector

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LlamacppMachineLearningConnector = void 0;
+var LlamacppMachineLearningConnector_1 = require("./models/LlamacppMachineLearningConnector");
+Object.defineProperty(exports, "LlamacppMachineLearningConnector", { enumerable: true, get: function () { return LlamacppMachineLearningConnector_1.LlamacppMachineLearningConnector; } });
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { LlamacppMachineLearningConnector } from './models/LlamacppMachineLearningConnector';
